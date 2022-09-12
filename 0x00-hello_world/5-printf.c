1d8ba222a72baf07b4c327e01c18971b76494111
bravine6 <ropbravine@gmail.com>
2022-09-12 03:14:57 -0700

    corrections

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+
+
+
 /**
+ *
  *  * main - Entry point
+ *
  *   *
- *    * Return: Always 0 (Success)
+ *
+ *    * Return: Always return 0 on success
+ *
  *     */
+
 int main(void)
+
 {
+
 		printf("with proper grammar, but the outcome is a piece of art,\n");
+
 			return (0);
+
 }
